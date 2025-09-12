259fe0184f7e6228c9db61d94a5dfd05c98a7d80
JoaoVictorTeixeira <106024649+Joao-Victor-Teixeira@users.noreply.github.com>
2025-09-12 07:59:28 -0300

    Finalização do Programa
    Aprendi que podemos chamar a Classe, e não precisar instanciar um objeto, assim como acontece com MATH.

--- curso_java/OOP/Membros_Estaticos/src/aplicacao/App.java
+++ curso_java/OOP/Membros_Estaticos/src/aplicacao/App.java
@@ -3,7 +3,7 @@ package aplicacao;
 import java.util.Locale;
 import java.util.Scanner;
 
-import util.Cauculador;
+import util.Calculador;
 
 public class App {
    
@@ -12,18 +12,18 @@ public class App {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Cauculador calc = new Cauculador();
+        
 
         System.out.print("Entre com o valor do raio: ");
         double raio = sc.nextDouble();
 
-        double c = calc.circunferencia(raio);
+        double c = Calculador.circunferencia(raio);
 
-        double v = calc.volume(raio);
+        double v = Calculador.volume(raio);
 
         System.out.printf("Circunferencia: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("Valor de PI: %.2f%n", calc.PI);    
+        System.out.printf("Valor de PI: %.2f%n", Calculador.PI);    
 
 
         sc.close();
