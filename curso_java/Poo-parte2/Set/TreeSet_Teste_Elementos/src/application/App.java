f696cdae5d09312e4314b390cb077084a4e04cb1
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-10-10 06:57:32 -0300

    Set comparando objetos
    Uma forma que retorna false pois não implementamos hashCode nem Equals para classe Product, fazendo o Set analizar por referencias e mesmo sendo 2 coisas iguais retorna false, pois por referencia são dois objestos diferentes

---
+++ curso_java/Poo-parte2/Set/TreeSet_Teste_Elementos/src/application/App.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import entities.Product;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Set<Product> set = new HashSet<>();
+
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.00));
+        set.add(new Product("Tablet", 400.0));
+
+        Product prod = new Product("Notebook", 1200.0);
+
+        System.out.println(set.contains(prod));
+
+    }
+}
