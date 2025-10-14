30922ed795aa393fc9d3a3dea9315866f83be39d
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-10-14 07:48:48 -0300

    Implementa Predicate com classe externa para filtrar produtos

---
+++ curso_java/Poo-parte2/Expressões Lambda/Predicate(removeIf)/src/application/App.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import util.ProductPredicate;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        List<Product> prod = new ArrayList<>();
+
+        prod.add(new Product("Tv", 900.0));
+        prod.add(new Product("Mouse", 50.0));
+        prod.add(new Product("Tablet", 350.0));
+        prod.add(new Product("HD Case", 80.90));
+
+        prod.removeIf(new ProductPredicate());
+
+        prod.forEach(System.out::println);
+    }
+}
