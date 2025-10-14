78966f7609af4d6325939d9713568920b6c9663f
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-10-13 22:21:25 -0300

    Aplica Method References para ordenação e iteração
    Utiliza Method References (::), uma forma compacta de expressão lambda que demonstra o conceito de funções como cidadãos de primeira classe em Java.
    
    A ordenação da lista de produtos agora é feita passando uma referência ao método estático App::compareProducts diretamente para o list.sort().
    
    A iteração e impressão da lista foram simplificadas, substituindo o loop for tradicional pela chamada list.forEach(System.out::println).

---
+++ curso_java/Poo-parte2/Expressões Lambda/Programação_Funcional_Calculo_Lambda/Inferência_Tipos/src/application/App.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class App {
+
+    public static int compareProducts(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
+    }
+    public static void main(String[] args) throws Exception {
+        
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.0));
+        list.add(new Product("Notebook", 1200.0));
+        list.add(new Product("Tablet", 4500.0));
+
+        list.sort(App:: compareProducts);
+        list.forEach(System.out::println);
+
+    }
+}
